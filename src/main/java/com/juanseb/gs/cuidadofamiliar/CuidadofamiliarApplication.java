153682087910c1d8f4740bf2796558c3016ce8b6
juansebgo98 <juan-sebastian2009@hotmail.es>
2024-05-15 15:22:21 +0200

    Proyecto Base

---
+++ src/main/java/com/juanseb/gs/cuidadofamiliar/CuidadofamiliarApplication.java
@@ -0,0 +1,13 @@
+package com.juanseb.gs.cuidadofamiliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidadofamiliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuidadofamiliarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanseb/gs/cuidadofamiliar/CuidadofamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanseb.gs.cuidadofamiliar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidadofamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
